345fd9fcf13db72f40b008c52ce578b4c2a592f6
victor.antunes <victor.antunes@gs3tecnologia.com.br>
2025-03-21 13:47:44 -0300

    Fixing Deputado model parameters

--- models/deputado_uri.py
+++ models/deputado_uri.py
@@ -1,50 +1,51 @@
 from datetime import date
 from typing import List, Optional
-from pydantic import BaseModel, HttpUrl
+from pydantic import BaseModel, HttpUrl, Field
 
 class Gabinete(BaseModel):
-    nome: Optional[str]
-    predio: Optional[str]
-    sala: Optional[str]
-    andar: Optional[str]
-    telefone: Optional[str]
-    email: Optional[str]
+    nome: Optional[str] = Field(default=None, allow_none=True)
+    predio: Optional[str] = Field(default=None, allow_none=True)
+    sala: Optional[str] = Field(default=None, allow_none=True)
+    andar: Optional[str] = Field(default=None, allow_none=True)
+    telefone: Optional[str] = Field(default=None, allow_none=True)
+    email: Optional[str]= Field(default=None, allow_none=True)
 
 class UltimoStatus(BaseModel):
     id: int
     uri: HttpUrl
-    nome: str
-    siglaPartido: str
-    uriPartido: Optional[HttpUrl]
-    siglaUf: str
+    nome: Optional[str] = Field(default=None, allow_none=True)
+    siglaPartido: Optional[str] = Field(default=None, allow_none=True)
+    uriPartido: Optional[HttpUrl] = Field(default=None, allow_none=True)
+    siglaUf: Optional[str] = Field(default=None, allow_none=True)
     idLegislatura: int
     urlFoto: HttpUrl
-    email: Optional[str]
-    data: date 
+    email: Optional[str] = Field(default=None, allow_none=True)
+    data: Optional[date] = Field(default=None, allow_none=True)
     nomeEleitoral: str
     gabinete: Gabinete
-    situacao: str
-    condicaoEleitoral: str
-    descricaoStatus: str
+    situacao: Optional[str] = Field(default=None, allow_none=True)
+    condicaoEleitoral: Optional[str] = Field(default=None, allow_none=True)
+    
+    descricaoStatus: Optional[str] = Field(default=None, allow_none=True)
 
 class Dados(BaseModel):
     id: int
     uri: HttpUrl
-    nomeCivil: str
+    nomeCivil: Optional[str] = Field(default=None, allow_none=True)
     ultimoStatus: UltimoStatus
-    cpf: str
-    sexo: str
-    urlWebsite: Optional[HttpUrl]
+    cpf: Optional[str] = Field(default=None, allow_none=True)
+    sexo: Optional[str] = Field(default=None, allow_none=True)
+    urlWebsite: Optional[str] = Field(default=None, allow_none=True)
     redeSocial: List[str]
-    dataNascimento: date
-    dataFalecimento: Optional[date] 
-    ufNascimento: str
-    municipioNascimento: str
-    escolaridade: str
+    dataNascimento: Optional[date] = Field(default=None, allow_none=True)
+    dataFalecimento: Optional[date] = Field(default=None, allow_none=True)
+    ufNascimento: Optional[str] = Field(default=None, allow_none=True)
+    municipioNascimento: Optional[str] = Field(default=None, allow_none=True)
+    escolaridade: Optional[str] = Field(default=None, allow_none=True)
 
 class Link(BaseModel):
-    rel: str
-    href: HttpUrl
+    rel: Optional[str] = Field(default=None, allow_none=True)
+    href: Optional[HttpUrl] = Field(default=None, allow_none=True)
 
 class DeputadoUri(BaseModel):
     dados: Dados
